turkeryilmaz/openairinterface5g

Add PDU session add log in E1 handler
bearer context modification and release print a message, so do the same
here as well.

openair2/LAYER2/nr_pdcp/cucp_cuup_handler.c
@@ -164,8 +164,10 @@ void e1_bearer_context_setup(const e1ap_bearer_setup_req_t *req)
   };
   resp.numPDUSessions = req->numPDUSessions;
   for (int i = 0; i < resp.numPDUSessions; ++i) {
-    pdu_session_setup_t *resp_pdu = resp.pduSession + i;
     const pdu_session_to_setup_t *req_pdu = req->pduSession + i;
+    LOG_I(E1AP, "UE %d: add PDU session ID %ld (%ld bearers)\n", cu_up_ue_id, req_pdu->sessionId, req_pdu->numDRB2Setup);
+
+    pdu_session_setup_t *resp_pdu = resp.pduSession + i;
     resp_pdu->id = req_pdu->sessionId;
 
     AssertFatal(req_pdu->numDRB2Setup == 1, "can only handle one DRB per PDU session\n");

dong881/openairinterface5g-NTUST

openair2/LAYER2/nr_pdcp/cucp_cuup_handler.c
@@ -163,8 +163,10 @@ void e1_bearer_context_setup(const e1ap_bearer_setup_req_t *req)
   };
   resp.numPDUSessions = req->numPDUSessions;
   for (int i = 0; i < resp.numPDUSessions; ++i) {
-    pdu_session_setup_t *resp_pdu = resp.pduSession + i;
     const pdu_session_to_setup_t *req_pdu = req->pduSession + i;
+    LOG_I(E1AP, "UE %d: add PDU session ID %ld (%d bearers)\n", cu_up_ue_id, req_pdu->sessionId, req_pdu->numDRB2Setup);
+
+    pdu_session_setup_t *resp_pdu = resp.pduSession + i;
     resp_pdu->id = req_pdu->sessionId;
 
     AssertFatal(req_pdu->numDRB2Setup == 1, "can only handle one DRB per PDU session\n");