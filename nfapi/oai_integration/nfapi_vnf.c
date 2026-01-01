3d0553d847b6529a2cabadd968b8d8a7a705cf42
dong881 <minghunghsu.taiwan@gmail.com>
2026-01-02 01:42:26 +0800

    add for rapp plot

--- nfapi/oai_integration/nfapi_vnf.c
+++ nfapi/oai_integration/nfapi_vnf.c
@@ -943,13 +943,20 @@ int oai_nfapi_ul_dci_req(nfapi_nr_ul_dci_request_t* ul_dci_req);
 int phy_nr_slot_indication(nfapi_nr_slot_indication_scf_t *ind)
 {
   LOG_D(MAC, "VNF SFN/Slot %d.%d \n", ind->sfn, ind->slot);
-
+  char print_info[128];
+  clock_gettime(CLOCK_MONOTONIC, &vnf_timing.trigger_sched_start);
   // this variable is very big (multiple MB), so we put it into static storage
   // to not overflow the stack while still having it in local (function) scope
   // also, phy_nr_slot_indication() is only executed by one thread, serially
   static NR_Sched_Rsp_t sched_response;
   NR_IF_Module_t *ifi = RC.nrmac[0]->if_inst;
   ifi->NR_slot_indication(ind, &sched_response);
+  clock_gettime(CLOCK_MONOTONIC, &vnf_timing.scheduler_end);
+  long sched_time_us = calc_elapsed_us(&vnf_timing.trigger_sched_start, &vnf_timing.scheduler_end);
+  if (sched_time_us > 0) {
+    snprintf(print_info, sizeof(print_info), "[SCHED]%ld", sched_time_us);
+    log_mmap_entry(4, ind->sfn, ind->slot, print_info);
+  }
 
 #ifdef ENABLE_AERIAL
     bool send_slt_resp = false;
@@ -973,17 +980,49 @@ int phy_nr_slot_indication(nfapi_nr_slot_indication_scf_t *ind)
       oai_fapi_send_end_request(0, ind->sfn, ind->slot);
     }
 #else
-  if (sched_response.DL_req.dl_tti_request_body.nPDUs > 0)
+  if (sched_response.DL_req.dl_tti_request_body.nPDUs > 0){
+    clock_gettime(CLOCK_MONOTONIC, &vnf_timing.dl_tti_pack_start);
     oai_nfapi_dl_tti_req(&sched_response.DL_req);
+    clock_gettime(CLOCK_MONOTONIC, &vnf_timing.dl_tti_pack_end);
+    long dl_tti_time_us = calc_elapsed_us(&vnf_timing.dl_tti_pack_start, &vnf_timing.dl_tti_pack_end);
+    if (dl_tti_time_us > 0) {
+      snprintf(print_info, sizeof(print_info), "[DL_TTI]%ld", dl_tti_time_us);
+      log_mmap_entry(4, ind->sfn, ind->slot, print_info);
+    }
+  }
 
-  if (sched_response.UL_tti_req.n_pdus > 0)
+  if (sched_response.UL_tti_req.n_pdus > 0){
+    clock_gettime(CLOCK_MONOTONIC, &vnf_timing.ul_tti_pack_start);
     oai_nfapi_ul_tti_req(&sched_response.UL_tti_req);
+    clock_gettime(CLOCK_MONOTONIC, &vnf_timing.ul_tti_pack_end);
+    long ul_tti_time_us = calc_elapsed_us(&vnf_timing.ul_tti_pack_start, &vnf_timing.ul_tti_pack_end);
+    if (ul_tti_time_us > 0) {
+      snprintf(print_info, sizeof(print_info), "[UL_TTI]%ld", ul_tti_time_us);
+      log_mmap_entry(4, ind->sfn, ind->slot, print_info);
+    }
+  }
 
-  if (sched_response.TX_req.Number_of_PDUs > 0)
+  if (sched_response.TX_req.Number_of_PDUs > 0){
+    clock_gettime(CLOCK_MONOTONIC, &vnf_timing.tx_data_pack_start);
     oai_nfapi_tx_data_req(&sched_response.TX_req);
+    clock_gettime(CLOCK_MONOTONIC, &vnf_timing.tx_data_pack_end);
+    long tx_data_time_us = calc_elapsed_us(&vnf_timing.tx_data_pack_start, &vnf_timing.tx_data_pack_end);
+    if (tx_data_time_us > 0) {
+      snprintf(print_info, sizeof(print_info), "[TX_DATA]%ld", tx_data_time_us);
+      log_mmap_entry(4, ind->sfn, ind->slot, print_info);
+    }
+  }
 
-  if (sched_response.UL_dci_req.numPdus > 0)
+  if (sched_response.UL_dci_req.numPdus > 0){
+    clock_gettime(CLOCK_MONOTONIC, &vnf_timing.ul_dci_pack_start);
     oai_nfapi_ul_dci_req(&sched_response.UL_dci_req);
+    clock_gettime(CLOCK_MONOTONIC, &vnf_timing.ul_dci_pack_end);
+    long ul_dci_time_us = calc_elapsed_us(&vnf_timing.ul_dci_pack_start, &vnf_timing.ul_dci_pack_end);
+    if (ul_dci_time_us > 0) {
+      snprintf(print_info, sizeof(print_info), "[UL_DCI]%ld", ul_dci_time_us);
+      log_mmap_entry(4, ind->sfn, ind->slot, print_info);
+    }
+  }
 #endif
 
   /* the below works because the function behind the callback collects
@@ -997,7 +1036,6 @@ int phy_nr_slot_indication(nfapi_nr_slot_indication_scf_t *ind)
   clock_gettime(CLOCK_MONOTONIC, &vnf_timing.trigger_sched_end);
   long total_time_us = calc_elapsed_us(&vnf_timing.trigger_sched_start, &vnf_timing.trigger_sched_end);
   if (total_time_us > 0) {
-    char print_info[64];
     snprintf(print_info, sizeof(print_info), "[TOTAL]%ld", total_time_us);
     log_mmap_entry(4, ind->sfn, ind->slot, print_info);
   }
