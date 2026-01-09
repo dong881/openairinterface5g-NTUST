3e81efc64006905fac52a4bc6fc62b4a2678ba42
dong881 <minghunghsu.taiwan@gmail.com>
2026-01-09 15:08:02 +0800

    feat: Add logging for learned margin in nr-softmodem initialization

--- executables/nr-softmodem.c
+++ executables/nr-softmodem.c
@@ -799,6 +799,7 @@ int main( int argc, char **argv ) {
   init_mmap_logger("NR_TIMING_INFO.txt");
   init_mmap_logger("ul_node_sync.txt");
   init_mmap_logger("nfapi_path.txt");
+  init_mmap_logger("learned_margin.txt");
   int ru_id, CC_id = 0;
   start_background_system();
 
