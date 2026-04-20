d8cedfcb68a62960c02f6dc513ca163d71b4497f
dong881 <minghunghsu.taiwan@gmail.com>
2026-04-20 19:57:28 +0800

    fix: remove unused vnf_dl_tbs-B logging for cleaner metrics

--- executables/nr-softmodem.c
+++ executables/nr-softmodem.c
@@ -880,7 +880,6 @@ static void init_vnf_mmap_loggers(void)
   init_mmap_logger("vnf_dl_harq_available-count.bin");
   init_mmap_logger("vnf_dl_cqi-idx.bin");
   init_mmap_logger("vnf_dl_mcs-idx.bin");
-  init_mmap_logger("vnf_dl_tbs-B.bin");
   init_mmap_logger("vnf_dl_rb_size-PRB.bin");
   init_mmap_logger("vnf_dl_snr-dB10.bin");
   init_mmap_logger("vnf_dl_harq_round-count.bin");
